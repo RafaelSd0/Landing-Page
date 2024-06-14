f68dec96850ace15622fa06a4f0625265afa1ece
RafaelSd0 <player08901@gmail.com>
2024-06-13 23:44:30 -0300

    Landing Page conteudo

---
+++ src/script.js
@@ -0,0 +1,7 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function comprar() {
+      window.alert("Opção de compra Indisponivel")
+  }
